8ceb043ffd973b446eb9268669fb9140cb0cc9ff
Mohsin <junejomohsin254@gmail.com>
2025-04-17 15:28:08 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './components/login'
+import Signup from './components/Signup'
+
+function App() {
+
+  return (
+    <>
+      <Login/>
+      <Signup/>
+    </>
+  )
+}
+
+export default App
